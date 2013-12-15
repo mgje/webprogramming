10497d221bc3a2222510578e7c4fe7a1b7ab4658
Martin Guggisberg <martin.guggisberg@gmail.com>
2013-12-15 09:29:13 +0100

    optimize for mobile access

--- processing/interaktion.js
+++ processing/interaktion.js
@@ -1,3 +1,3 @@
-Processing_Code="void setup() {\n  size(600,500);\n  background(0);\n  noStroke();\n}\n\nvoid draw() {\n\n  if (mousePressed) {\n    background(0);\n  }\n\n  fill(222,221,3,9);\n  ellipse(mouseX, mouseY, 70, 70);\n}";
+Processing_Code="void setup() {\n  size(500,500);\n  background(0);\n  noStroke();\n}\n\nvoid draw() {\n\n  fill(222,221,3,9);\n  ellipse(mouseX, mouseY, 70, 70);\n}";
 $("a[data-index=3]").addClass("active");
 	
