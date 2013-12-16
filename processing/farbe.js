4801a12900fc6ab485470db8d32fe7be900896c6
Martin Guggisberg <Martin.guggisberg@gmail.com>
2013-12-16 14:50:09 +0100

    Update farbe.js

--- processing/farbe.js
+++ processing/farbe.js
@@ -1,3 +1,3 @@
-Processing_Code='//Wie entstehen Farben?\n\nsmooth();\nbackground(255);\nnoStroke();\n\nfill(255,0,0);\nellipse(60,60,32,32);\n\nfill(127,0,0);\nellipse(100,60,32,32);\n\nfill(255,200,200);\nellipse(140,60,32,32);\n\nfill(255,0,0);\nellipse(80,120,32,32);\n\nfill(127,0,0);\nellipse(120,120,32,32);\n\nfill(255,200,200);\nellipse(160,120,32,32);';
+Processing_Code='//Wie entstehen Farben?\n\nsmooth();\nbackground(255);\nnoStroke();\n\nfill(255,0,0);\nsize(500,445);\nellipse(60,60,32,32);\n\nfill(127,0,0);\nellipse(100,60,32,32);\n\nfill(255,200,200);\nellipse(140,60,32,32);\n\nfill(255,0,0);\nellipse(80,120,32,32);\n\nfill(127,0,0);\nellipse(120,120,32,32);\n\nfill(255,200,200);\nellipse(160,120,32,32);';
 // $(".lessonButton").removeClass("active");
 $("a[data-index=2]").addClass("active");
