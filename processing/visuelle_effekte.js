fa72c2be5afd9f43e5324f9633e806cc99912946
Martin Guggisberg <Martin.guggisberg@gmail.com>
2013-12-16 14:26:49 +0100

    Update visuelle_effekte.js

--- processing/visuelle_effekte.js
+++ processing/visuelle_effekte.js
@@ -1,2 +1,2 @@
-Processing_Code="void setup(){\n  stroke(255);\n  strokeWeight(4);\n  background(0,0,0);\n}\nvoid draw(){\n  var dx = random(-100,100);\n  var dy = random(-100,100);\n  stroke(random(220), random(155), random(100));\n  line(mouseX,mouseY,mouseX+dx,mouseY+dy);\n  fill (0, 0, 0, 10);\n  noStroke();\n  rect (0, 0, width, height);\n}";
+Processing_Code="void setup(){\n  stroke(255);\n  strokeWeight(4);\n  background(0,0,0);\n  size(500,445);\n}\nvoid draw(){\n  var dx = random(-100,100);\n  var dy = random(-100,100);\n  stroke(random(220), random(155), random(100));\n  line(mouseX,mouseY,mouseX+dx,mouseY+dy);\n  fill (0, 0, 0, 10);\n  noStroke();\n  rect (0, 0, width, height);\n}";
 $("a[data-index=5]").addClass("active");
