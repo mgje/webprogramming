2b07d486cc70152080e33aa4e57b65d2ad53e797
Martin Guggisberg <Martin.guggisberg@gmail.com>
2013-12-16 14:52:30 +0100

    Update interaktion.js

--- processing/interaktion.js
+++ processing/interaktion.js
@@ -1,3 +1,3 @@
-Processing_Code="//Programmieren Sie einen neuen Pinsel.\n\nvoid setup() {\n  size(500,500);\n  background(0);\n  noStroke();\n}\n\nvoid draw() {\n\n  fill(222,221,3,9);\n  ellipse(mouseX, mouseY, 70, 70);\n}";
+Processing_Code="//Programmieren Sie einen neuen Pinsel.\n\nvoid setup() {\n  size(500,445);\n  background(0);\n  noStroke();\n}\n\nvoid draw() {\n\n  fill(222,221,3,9);\n  ellipse(mouseX, mouseY, 70, 70);\n}";
 $("a[data-index=3]").addClass("active");
 	
